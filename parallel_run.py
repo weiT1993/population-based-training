dfc34474decd22986e4a1ecd713f853614243063
weiT1993 <tangwei1027@gmail.com>
2020-02-23 03:34:21 -0500

    built evolution framework

--- parallel_run.py
+++ parallel_run.py
@@ -4,28 +4,54 @@ from mpi4py import MPI
 
 def evolve(comm, worker, generation, steps):
     population_size = comm.Get_size()
-    worker.train(steps=steps)
 
     if worker.idx == population_size-1:
+        print('-'*30,'Generation %d START'%generation,'-'*30,flush=True)
+        for i in range(population_size-1):
+            comm.send('Generation %d START'%generation, dest=i)
+        print('Generation %d: Worker-%d START with %.3f'%(generation,worker.idx,worker.score),flush=True)
+        worker.train(steps=steps)
+
         max_score = worker.score
         max_idx = worker.idx
         for i in range(population_size-1):
             state = MPI.Status()
-            worker_idx, worker_score = comm.recv(source=MPI.ANY_SOURCE,status=state)
+            worker_generation, worker_idx, worker_score = comm.recv(source=MPI.ANY_SOURCE,status=state)
+            assert worker_generation == generation
             if worker_score>max_score:
                 max_score = worker_score
                 max_idx = worker_idx
+        
         for i in range(population_size-1):
-            comm.send((max_idx,max_score), dest=i)
+            comm.send((generation,max_idx,max_score), dest=i)
     else:
-        comm.send((worker.idx,worker.score), dest=population_size-1)
         state = MPI.Status()
-        max_idx, max_score = comm.recv(source=population_size-1,status=state)
+        start_signal = comm.recv(source=population_size-1,status=state)
+        assert start_signal == 'Generation %d START'%generation
+        print('Generation %d: Worker-%d START with %.3f'%(generation,worker.idx,worker.score),flush=True)
+        worker.train(steps=steps)
+
+        comm.send((generation,worker.idx,worker.score), dest=population_size-1)
+
+        state = MPI.Status()
+        master_generation, max_idx, max_score = comm.recv(source=population_size-1,status=state)
+        assert master_generation == generation
     
     if worker.idx != max_idx:
         worker.exploit(max_score=max_score)
         worker.explore()
         print('Generation %d: Worker-%d inherited from worker-%d = %.3f, explore'%(generation,worker.idx,max_idx,max_score),flush=True)
+    
+    if worker.idx == population_size-1:
+        for i in range(population_size-1):
+            state = MPI.Status()
+            end_signal = comm.recv(source=MPI.ANY_SOURCE,status=state)
+            if end_signal != 'Generation %d DONE'%generation:
+                raise Exception('Illegal end_signal : ',end_signal)
+        print('-'*30,'Generation %d DONE'%generation,'-'*30,flush=True)
+    else:
+        comm.send('Generation %d DONE'%generation, dest=population_size-1)
+        print('Generation %d: Worker-%d DONE with %.3f'%(generation,worker.idx,worker.score),flush=True)
 
 if __name__ == '__main__':
     comm = MPI.COMM_WORLD
@@ -35,10 +61,4 @@ if __name__ == '__main__':
     worker = Worker(idx=rank)
 
     for i in range(3):
-        if worker.idx == size-1:
-            print('-'*30,'Generation %d begins'%i,'-'*30,flush=True)
-        
         evolve(comm=comm, worker=worker, generation=i, steps=20)
-        
-        if worker.idx == size-1:
-            print('-'*30,'Generation %d DONE'%i,'-'*30,flush=True)
