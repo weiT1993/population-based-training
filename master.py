9620cca57662b52aaae478f24637e5ad811df89a
weiT1993 <tangwei1027@gmail.com>
2020-02-25 02:58:06 -0500

    working on explore

--- master.py
+++ master.py
@@ -1,5 +1,6 @@
 import argparse
 import subprocess
+from utils.helper_fun import read_file
 
 def train(num_workers):
     child_processes = []
@@ -10,11 +11,12 @@ def train(num_workers):
     for cp in child_processes:
         cp.wait()
 
-def explore(num_workers):
+def explore(num_workers,best_worker_idx):
     child_processes = []
     for i in range(num_workers):
-        p = subprocess.Popen(args=['python', 'worker.py','--idx','%d'%i,'--phase','explore'])
-        child_processes.append(p)
+        if i != best_worker_idx:
+            p = subprocess.Popen(args=['python', 'worker.py','--idx','%d'%i,'--phase','explore','--best-idx','%d'%best_worker_idx])
+            child_processes.append(p)
     
     for cp in child_processes:
         cp.wait()
@@ -28,15 +30,27 @@ def exploit(num_workers):
     for cp in child_processes:
         cp.wait()
 
+def find_best_worker():
+    scores = read_file(filename='./population/scores.p')
+    best_score = 0
+    best_worker = -1
+    for worker_idx in scores:
+        if scores[worker_idx]>best_score:
+            best_score = scores[worker_idx]
+            best_worker = worker_idx
+    subprocess.run(args=['rm','./population/scores.p'])
+    return best_worker, best_score
+
 def evolve(generation,num_workers,max_generations):
     print('*'*50,'Generation %d START'%generation,'*'*50,flush=True)
     train(num_workers=num_workers)
     if generation<max_generations-1:
-        explore(num_workers=num_workers)
-        exploit(num_workers=num_workers)
+        best_worker, best_score = find_best_worker()
+        print('Worker-%d has the best score = %.5f'%(best_worker,best_score))
+        explore(num_workers=num_workers,best_worker_idx=best_worker)
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='WOrker instance')
+    parser = argparse.ArgumentParser(description='Worker instance')
     parser.add_argument('--num-workers', metavar='N', type=int,help='Number of parallel workers')
     parser.add_argument('--num-generations', metavar='N', type=int,help='Number of generations')
     args = parser.parse_args()
--- worker.py
+++ worker.py
@@ -10,10 +10,11 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Worker instance')
     parser.add_argument('--idx', metavar='N', type=int,help='Worker Index')
     parser.add_argument('--phase', type=str, choices=['init','train','explore','exploit'])
+    parser.add_argument('--best-idx', metavar='N', default=-1, type=int,help='Best Worker Index')
     args = parser.parse_args()
 
     if args.phase == 'init':
-        print('Worker %d instantiated'%args.idx,flush=True)
+        print('Worker-%d instantiated'%args.idx,flush=True)
         worker = FC(worker_idx=args.idx)
         worker.random_model(num_layers=3)
         worker.save_model()
@@ -25,8 +26,15 @@ if __name__ == '__main__':
         worker.train(dataset_train=data_dict['train'],dataset_valid=data_dict['valid'])
         worker.save_model()
         new_score = worker.score
-        print('Worker %d score trained from %.5f --> %.5f'%(args.idx,old_score,new_score),flush=True)
+        print('Worker-%d score trained from %.5f --> %.5f'%(args.idx,old_score,new_score),flush=True)
+        # NOTE: avoid racing
+        time.sleep(random.random())
+        pickle.dump({args.idx:new_score},open('./population/scores.p','ab'))
     elif args.phase == 'explore':
-        print('Worker %d explore'%args.idx,flush=True)
+        best_worker = FC(worker_idx=args.idx)
+        data_dict = read_file(filename='./data/dataset.p')
+        best_worker.load_model(model_h5='./population/worker_%d.h5'%args.best_idx,dataset_valid=data_dict['valid'])
+        best_score = best_worker.score
+        print('Worker-%d explore best_score = %.5f'%(args.idx,best_score),flush=True)
     else:
-        print('Worker %d exploit'%args.idx,flush=True)
+        print('Worker-%d exploit'%args.idx,flush=True)
