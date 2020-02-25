436a937a58a28bf1a2272cf64e681134c2c304e5
weiT1993 <tangwei1027@gmail.com>
2020-02-25 00:48:20 -0500

    added save build, need to save_model and save_structure

--- models.py
+++ models.py
@@ -1,114 +1,62 @@
 import tensorflow as tf
 import random
+import pickle
+import time
 
 class FC():
-    def __init__(self, architecture):
-        if 'structure' not in architecture:
-            self.model = self.random_model(architecture['num_layers'])
+    def __init__(self, structure, worker_idx):
+        self.worker_idx = worker_idx
+        if 'layers' not in structure:
+            self.model = self.random_model(structure['num_layers'])
         else:
-            self.model = self.build_model(architecture['structure'])
+            self.model = self.build_model(structure)
     
     def random_model(self, num_layers):
-        activations = ['sigmoid','selu','relu',tf.nn.leaky_relu]
-        model = tf.keras.models.Sequential()
+        activations = [tf.nn.selu,tf.nn.relu,tf.nn.leaky_relu]
+        model = tf.keras.models.Sequential(name='worker_%d'%self.worker_idx)
         input_layer = tf.keras.layers.Flatten(input_shape=(300, 2),name='input_layer')
         model.add(input_layer)
         for i in range(num_layers):
-            dense_layer = tf.keras.layers.Dense(random.randint(10,600), activation=random.choice(activations))
+            dense_layer = tf.keras.layers.Dense(random.randint(10,600),activation=random.choice(activations),name='dense%d'%i)
             model.add(dense_layer)
         model.add(tf.keras.layers.Dense(2,activation='softmax',name = 'output_layer'))
         optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
         model.compile(optimizer=optimizer,
         loss=tf.losses.SparseCategoricalCrossentropy(from_logits=True),
         metrics=['accuracy'])
+        self.score = -1
         return model
     
     def build_model(self, structure):
-        model = tf.keras.models.Sequential()
+        model = tf.keras.models.Sequential(name='worker_%d'%self.worker_idx)
         input_layer = tf.keras.layers.Flatten(input_shape=(300, 2),name='input_layer')
         model.add(input_layer)
-        for layer in structure['layers']:
-            dense_layer = tf.keras.layers.Dense(layer['kernel_size'], activation=layer['activation'])
+        for i, layer in enumerate(structure['layers']):
+            dense_layer = tf.keras.layers.Dense(layer['kernel_size'], activation=layer['activation'],name=layer['name'])
             model.add(dense_layer)
         model.add(tf.keras.layers.Dense(2,activation='softmax',name = 'output_layer'))
         optimizer = tf.keras.optimizers.Adam(learning_rate=structure['lr'])
         model.compile(optimizer=optimizer,
         loss=tf.losses.SparseCategoricalCrossentropy(from_logits=True),
         metrics=['accuracy'])
+        self.score = structure['score']
         return model
 
-def explore_FCNN(good_model, bad_model, worker_idx):
-    good_layers = good_model.layers
-    bad_layers = bad_model.layers
-    input_shape = (good_layers[0].input_shape[1],good_layers[0].input_shape[2])
-
-    explore_model = tf.keras.models.Sequential()
-
-    layer_ctr = 1
-    for good_l, bad_l in zip(good_layers,bad_layers):
-        if 'input' in good_l.name:
-            explore_layer = good_l
-            explore_model.add(explore_layer)
-            layer_ctr += 1
-            continue
-        elif 'output' in good_l.name:
-            explore_output_shape = 2
-            layer_name = 'output_layer_%d'%worker_idx
-        else:
-            explore_output_shape = max(abs(int(0.8*(good_l.output_shape[1] - bad_l.output_shape[1])) + good_l.output_shape[1]),10)
-            layer_name = 'dense%d_%d'%(layer_ctr,worker_idx)
-        # print('Layer-%d, good-%d, bad-%d, explore-%d'%(layer_ctr,good_l.output_shape[1],bad_l.output_shape[1],explore_output_shape))
-        
-        explore_layer = tf.keras.layers.Dense(explore_output_shape,
-        activation=good_l.activation,
-        name=layer_name,
-        kernel_initializer=tf.keras.initializers.Zeros())
-        explore_model.add(explore_layer)
-        
-        good_kernel = good_l.weights[0].numpy()
-        explore_kernel = explore_layer.weights[0].numpy()
-        for i in range(explore_kernel.shape[0]):
-            for j in range(explore_kernel.shape[1]):
-                if i < good_kernel.shape[0] and j<good_kernel.shape[1]:
-                    explore_kernel[i][j] = good_kernel[i][j]
-                else:
-                    explore_kernel[i][j] = 0
-        explore_layer.weights[0].assign(explore_kernel)
-
-        good_bias = good_l.weights[1].numpy()
-        explore_bias = explore_layer.weights[1].numpy()
-        for i in range(explore_bias.shape[0]):
-            if i < good_bias.shape[0]:
-                explore_bias[i] = good_bias[i]
-            else:
-                explore_bias[i] = 0
-        explore_layer.weights[1].assign(explore_bias)
-        layer_ctr += 1
-
-    optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
-
-    explore_model.compile(optimizer=optimizer,
-    loss=tf.losses.SparseCategoricalCrossentropy(from_logits=True),
-    metrics=['accuracy'])
-
-    return explore_model
-
-def create_CNN(num_layers, worker_idx):
-    activations = ['sigmoid','selu','relu',tf.nn.leaky_relu]
-    model = tf.keras.models.Sequential()
-    model.add(tf.keras.layers.Conv1D(filters=32, kernel_size=3, activation=random.choice(activations), input_shape=(2, 300)))
-    # model.add(tf.keras.layers.AveragePooling1D(pool_size=2))
-    # model.add(tf.keras.layers.Conv1D(filters=16, kernel_size=50, activation=random.choice(activations)))
-    # model.add(tf.keras.layers.AveragePooling1D(pool_size=2))
-    # model.add(tf.keras.layers.Conv1D(filters=8, kernel_size=20, activation=random.choice(activations)))
-    model.add(tf.keras.layers.Flatten())
-    model.add(tf.keras.layers.Dense(2, activation='softmax'))
-
-    optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
-    model.compile(optimizer=optimizer,
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-              metrics=['accuracy'])
-    return model
-
-def explore_CNN(good_model, bad_model, worker_idx):
-    return good_model
+    def save_model(self):
+        layers = self.model.layers
+        structure = {}
+        structure['layers'] = []
+        for layer in layers:
+            if 'dense' in layer.name:
+                structure['layers'].append({'kernel_size':layer.output_shape[1],
+                'activation':layer.activation,
+                'name':layer.name})
+        structure['lr'] = self.model.optimizer.learning_rate.numpy()
+        structure['score'] = self.score
+        pickle.dump(structure,open('./population/worker_%d.p'%self.worker_idx,'wb'))
+    
+    def train(self,dataset_train,dataset_valid):
+        x_train, y_train = dataset_train
+        x_valid, y_valid = dataset_valid
+        self.model.fit(x_train,y_train,epochs=3,verbose=0)
+        self.score = self.model.evaluate(x_valid,y_valid,verbose=0)[1]
--- worker.py
+++ worker.py
@@ -1,7 +1,10 @@
 import time
+import random
 import argparse
+import pickle
 import tensorflow as tf
 from models import FC
+from utils.helper_fun import read_file
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Worker instance')
@@ -11,9 +14,17 @@ if __name__ == '__main__':
 
     if args.phase == 'init':
         print('Worker %d instantiated'%args.idx,flush=True)
-        worker = FC(architecture={'num_layers':3})
+        worker = FC(structure={'num_layers':3},worker_idx=args.idx)
+        worker.save_model()
     elif args.phase == 'train':
-        print('Worker %d train'%args.idx,flush=True)
+        structure = pickle.load(open('./population/worker_%d.p'%args.idx,'rb'))
+        worker = FC(structure=structure,worker_idx=args.idx)
+        old_score = worker.score
+        data_dict = read_file(filename='./data/dataset.p')
+        worker.train(dataset_train=data_dict['train'],dataset_valid=data_dict['valid'])
+        worker.save_model()
+        new_score = worker.score
+        print('Worker %d score trained from %.5f --> %.5f'%(args.idx,old_score,new_score),flush=True)
     elif args.phase == 'explore':
         print('Worker %d explore'%args.idx,flush=True)
     else:
