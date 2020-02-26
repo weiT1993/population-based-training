59d33e70b67b48ab7637e8d60864efa4c9eaa9ba
weiT1993 <tangwei1027@gmail.com>
2020-02-26 16:29:03 -0500

    suppress all W, I, E logging

--- run.sh
+++ run.sh
@@ -7,4 +7,4 @@ if [ ! -d "./$NUM_LAYERS-$MODEL-population" ]; then
   mkdir ./$NUM_LAYERS-$MODEL-population
 fi
 
-python master.py --model $MODEL --num-workers $NUM_WORKERS --num-layers $NUM_LAYERS --max-generations $MAX_GENERATIONS 2>&1 | tee ./$NUM_LAYERS-$MODEL-population/logs.txt
+python master.py --model $MODEL --num-workers $NUM_WORKERS --num-layers $NUM_LAYERS --max-generations $MAX_GENERATIONS | tee > ./$NUM_LAYERS-$MODEL-population/logs.txt 2> ./$NUM_LAYERS-$MODEL-population/err.txt
