88c7ad6e06dc0d4db3c080fdca4b8c4dfbabf781
weiT1993 <tangwei1027@gmail.com>
2020-02-26 12:44:25 -0500

    debug

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-python master.py --num-workers 50 --num-generations 1000 2> err_logs.txt 1> output.txt
+python master.py --num-workers 50 --num-layers 5 --num-generations 1000 2> err_logs.txt 1> output.txt
