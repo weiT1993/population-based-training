50e8dd01de46e1f5d36312be88c3f3733c844f5a
weiT1993 <tangwei1027@gmail.com>
2020-04-11 04:06:06 -0400

    debugging IO

--- utils/datasets.py
+++ utils/datasets.py
@@ -7,13 +7,20 @@ from utils.helper_fun import read_mat
 
 # def get_dataset(data_file,time_range,concat):
 #     X, y = read_mat(file_name=data_file,time_range=time_range,concat=concat)
+#     print(X.shape)
 #     X = np.transpose(X, (0,2,1))
+#     print(X.shape)
 #     X_train, X_remaining, y_train, y_remaining = train_test_split(X, y, test_size=0.4)
 #     dataset_train = (X_train, y_train)
 
 #     X_valid, X_test, y_valid, y_test = train_test_split(X_remaining, y_remaining, test_size=0.5)
 #     dataset_valid = (X_valid, y_valid)
 #     dataset_test = (X_test, y_test)
+
+#     print(X_train.shape,y_train.shape)
+#     print(X_valid.shape,y_valid.shape)
+#     print(X_test.shape,y_test.shape)
+
 #     return dataset_train, dataset_valid, dataset_test
 
 # dataset_train, dataset_valid, dataset_test = get_dataset(data_file='./data/power_15freq_7.3202.mat',time_range=300,concat=False)
@@ -22,6 +29,7 @@ from utils.helper_fun import read_mat
 def get_dataset(data_file,dt=8000):
     data = np.load(data_file)
     data = data.astype(np.float)
+    print(data.shape)
     [excitedI, excitedQ, groundI, groundQ, groundI_test, groundQ_test] = data
 
     train_idx = int(len(excitedI)*0.6)
